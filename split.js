365d741beed2878cd608f3dd5f4763109f32c6ab
MdTaufiqurRahman <taufiqur.anik.bd@gmail.com>
2021-02-25 18:19:55 +0600

    practice

---
+++ split.js
@@ -0,0 +1,9 @@
+const nums = [1, 5, 6, 2, 8 ,9 ,55, 44];
+
+// const result = nums.slice(2, 5);
+
+//const result = nums.splice(2,4, 44,77);
+
+const result = nums.join(". Anik, ");
+
+console.log(result);
